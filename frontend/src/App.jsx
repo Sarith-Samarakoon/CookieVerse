6e0501ae0a4e131a632a258c9ad42f90ff7d7a1e
Sarith-Samarakoon <IT22207036@my.sliit.lk>
2025-04-30 01:19:18 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,11 @@
-import React from "react"; 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; 
-import Home from "./components/Home"; 
-import Login from "./components/Login"; 
-import Register from "./components/Register"; 
-import ProtectedRoute from "./components/ProtectedRoute"; 
-// import CommunityPage from "./components/CommunityPage"; // Import the CommunityPage component
-// import LeaningPage from "./components/PlanManagement"
-import ProfilePage from "./components/UserProfile"
-// import CreateLearningPlan from "./components/CreateLearningPlan";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import ProtectedRoute from "./components/ProtectedRoute";
+import ProfilePage from "./components/UserProfile";
+
 import "./index.css";
 
 function App() {
@@ -24,10 +22,7 @@ function App() {
         />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        {/* <Route path="/community/:id" element={<CommunityPage />} />
-        <Route path="/learning" element={<LeaningPage />} /> */}
         <Route path="/profile" element={<ProfilePage />} />
-        {/* <Route path="/create-learning-plan" element={<CreateLearningPlan />} /> */}
       </Routes>
     </Router>
   );
