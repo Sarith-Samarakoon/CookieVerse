bd2813936b10175c6ce5408992c51a20cf385f76
chalani1224 <135096414+chalani1224@users.noreply.github.com>
2025-04-30 10:50:23 +0530

    backend toggle like function

---
+++ backend/src/main/java/com/example/backend/controller/LikeCommentController.java
@@ -0,0 +1,29 @@
+package com.example.backend.controller;
+
+import com.example.backend.model.LikeComment;
+import com.example.backend.service.LikeCommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/likecomment")
+public class LikeCommentController {
+
+    @Autowired
+    private LikeCommentService likeCommentService;
+
+    @PostMapping("/toggle-like/{postId}")
+    public ResponseEntity<String> toggleLike(
+            @PathVariable String postId,
+            @RequestHeader("userId") String userId,
+            @RequestHeader("username") String username) {
+        likeCommentService.toggleLike(postId, userId, username);
+        return ResponseEntity.ok("Like status toggled successfully.");
+    }
+
+}
